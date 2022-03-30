18cec30c261745042dce959fdeff99ff22da7f42
Tainá Vilela <tainadantas89@hotmail.com>
2022-03-30 18:25:50 -0300

    aguardando o bd

--- controllers/cadastro.js
+++ controllers/cadastro.js
@@ -1,13 +1,8 @@
-const Cadastro = require('../models')
-
 module.exports = app =>{
 
     app.get('/cadastros', (req, res) => res.send('rota cadastro fazendo get'))
 
     app.post('/cadastros', (req, res) => {
-        const cadastros = req.body
 
-        Cadastro.create(cadastros, res)
-
-    })
+        res.send('rota cadastro realizando post')})
 }

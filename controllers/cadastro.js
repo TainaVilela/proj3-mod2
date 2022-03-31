10b82bdc09c433ef53a27099bf4f2e57071d460e
Tainá Vilela <tainadantas89@hotmail.com>
2022-03-30 21:20:37 -0300

    criando rotas no index

--- controllers/cadastro.js
+++
@@ -1,8 +0,0 @@
-module.exports = app =>{
-
-    app.get('/cadastros', (req, res) => res.send('rota cadastro fazendo get'))
-
-    app.post('/cadastros', (req, res) => {
-
-        res.send('rota cadastro realizando post')})
-}
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const consign = require('consign');
-
+const path = require("path");
+require('dotenv').config();
 const bodyParser = require('body-parser');
 
-const cadastroRoute = require("./routes/cadastroRoute");
+//const Filme = require('./models')
 
 module.exports = () => {
 
@@ -11,16 +11,124 @@ module.exports = () => {
 
   app.use(express.json());
 
+  app.use(express.static(path.join(__dirname, "public")));
+
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
 
-  app.set("view engine", "ejs");
+  app.get("/", async (req, res) => {
+    const filmes = await Filme.findAll();
+    console.log(filmes);
+    console.log(typeof(filmes));
+    res.render("index", {
+      filmes,
+    });
+  });
 
-  app.use('/cadastro', cadastroRoute)
+  app.get("/filmes/:id", async (req, res) => {
+    const filme = await Filme.findByPk(req.params.id);
 
-  consign()
-      .include('controllers')
-      .into(app)
+    res.render("detalhes", {
+      filme,
+    });
+  });
+
+  app.get("/criar", (req, res) => {
+    res.render("criar",{mensagem: ""});
+  });
+
+  app.post("/criar", async (req, res) => {
+    const { nome, descricao, imagem } = req.body;
+
+    if (!nome) {
+      res.render("criar", {
+        mensagem: "Nome é obrigatório",
+      });
+    }
+
+    if (!imagem) {
+      res.render("criar", {
+        mensagem: "Imagem é obrigatório",
+      });
+    }
+
+    try {
+      const filme = await Filme.create({
+        nome,
+        descricao,
+        imagem,
+      });
+
+      res.render("criar", {
+        filme,
+        mensagem: ""
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.render("criar", {
+        mensagem: "Ocorreu um erro ao cadastrar o Filme!",
+      });
+    }
+  });
+
+  app.get("/editar/:id", async (req, res) => {
+    const filme = await Filme.findByPk(req.params.id);
+
+    if (!filme) {
+      res.render("editar", {
+        mensagem: "Filme não encontrado!",
+      });
+    }
+
+    res.render("editar", {
+      filme, mensagem: ""
+    });
+  });
+
+  app.post("/editar/:id", async (req, res) => {
+    const filme = await Filme.findByPk(req.params.id);
+
+    const { nome, descricao, imagem } = req.body;
+
+    filme.nome = nome;
+    filme.descricao = descricao;
+    filme.imagem = imagem;
+
+    const filmeEditado = await filme.save();
+
+    res.render("editar", {
+      filme: filmeEditado,
+      mensagem: "Filme editado com sucesso!",
+    });
+  });
+
+  app.get("/deletar/:id", async (req, res) => {
+    const filme = await Filme.findByPk(req.params.id);
+
+    if (!filme) {
+      res.render("deletar", {
+        mensagem: "Filme não encontrado!",
+      });
+    }
+
+    res.render("deletar", {
+      filme,mensagem: ""
+    });
+  });
+
+  app.post("/deletar/:id", async (req, res) => {
+    const filme = await Filme.findByPk(req.params.id);
+
+    if (!filme) {
+      res.render("deletar", {
+        mensagem: "Filme não encontrado!",
+      });
+    }
+
+    await filme.destroy();
+    res.redirect("/");
+  });
 
   return app;
 };

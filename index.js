44a82dfb9dce6f498c6ed2b70d7d10651a4d3e77
Tainá Vilela <tainadantas89@hotmail.com>
2022-04-04 12:38:36 -0300

    trocando a pasta do servidor

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const path = require("path");
 
 require("dotenv").config();
 const Filme = require("./model/filmes");
-
 const db = require('./model/database');
-
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
@@ -23,13 +21,11 @@ const port = process.env.PORT || 3000;
   
 
   app.get("/", async (req, res) => {
-
     //  const filmes = await Filme.findAll();
     //  console.log(filmes);
     //  console.log(typeof filmes);
     res.render("index", {
       //filmes,
-
     });
   });
 
@@ -37,17 +33,9 @@ const port = process.env.PORT || 3000;
     res.render("lista");
   });
 
-<<<<<<< HEAD
   app.get("/detalhes",(req, res)=>{
     res.render("detalhes");
-=======
-
-  app.get("/login",(req, res)=>{
-    res.render("login");
->>>>>>> 6277adc7e8393fd288ead671452e4320f90fff26
   });
-
-
 
   app.get("/filmes/:id", async (req, res) => {
     const filme = await Filme.findByPk(req.params.id);
@@ -156,11 +144,7 @@ const port = process.env.PORT || 3000;
     res.redirect("/");
   });
 
-
 db.conectado();
-<<<<<<< HEAD
 
 app.listen(port, () =>
     console.log(`Servidor rodando em http://localhost:${port}`))
-=======
->>>>>>> 6277adc7e8393fd288ead671452e4320f90fff26

77b322cbad5b064551557be6f9ab3f0536e2deb5
Tainá Vilela <tainadantas89@hotmail.com>
2022-04-05 20:00:29 -0300

    adicionando rota create

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ const routes = require("express").Router();
 const filmeController = require("../controllers/filmeController");
 
 routes.get('/', filmeController.getAll);
+routes.post('/create', filmeController.create);
 routes.get('/getById/:id/:method', filmeController.getById);
 routes.post('/update/:id', filmeController.update);
 routes.get('/remove/:id', filmeController.remove);
